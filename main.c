3c811bfb85cf93aa76b4cf494e755bb92a5f9b2c
BREBION Mathis <mathis.brebion@etu.unilasalle.fr>
2022-05-18 08:28:25 +0200

    main

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+
+#define ARRAY_LENGTH( array ) ( sizeof ( array ) / sizeof ( *array ) )
+
+
+int main ( void ) {
+
+
+
+    printf ( "\n\n" );
+    system ( "PAUSE" );
+    return EXIT_SUCCESS;
+}
